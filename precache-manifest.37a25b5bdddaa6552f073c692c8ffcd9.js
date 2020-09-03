1816c7666bf1e911d69ca14aa0e1b2dc9dee3232
Balamalynur-Media <balamalynur.media09@gmail.com>
2020-09-04 03:29:05 +0530

    Updates

---
+++ precache-manifest.37a25b5bdddaa6552f073c692c8ffcd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fb3c6a5540115ff9bd05c6cba3e7f3",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "eda5347dc5bbc11a94f2",
+    "url": "/github-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a39f0596ac6a09afa4e0",
+    "url": "/github-pages/static/js/2.e1e8a0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages/static/js/2.e1e8a0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda5347dc5bbc11a94f2",
+    "url": "/github-pages/static/js/main.63b8d33c.chunk.js"
+  },
+  {
+    "revision": "0ac8eddcb8c2cda97027",
+    "url": "/github-pages/static/js/runtime-main.bc9f5325.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
